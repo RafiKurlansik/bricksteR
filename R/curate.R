39866f6a886e4f08b80721d87b03ba2b399a43fa
rafi.kurlansik <rafi.kurlansik@databricks.com>
2021-02-22 12:49:12 -0500

    Adding curate, set_library, remove_library

---
+++ R/curate.R
@@ -0,0 +1,88 @@
+#'
+#' Curate a shared or personal library of packages
+#'
+#' Helper function to manage package installation across libraries
+#'
+#' `curate()` lets you easily install packages into different libraries.  It will
+#' manage the copying of packages to a path on DBFS, which will persist them from
+#' session to session on Databricks.
+#'
+#' @param pkg String, name of package to install or a valid URL if using GitHub/Gitlab
+#' @param repo Where to pull the package from.  Default is RStudio Package Manager
+#' (https://packagemanager.rstudio.com/all/__linux__/xenial/latest).  If NULL, specify
+#' the path to a source file in `pkgs`.
+#' @param version The desired version of the package to install.  Defaults to latest.
+#' @param lib String, the path to the library where packages will be installed.  Default
+#' is the first path on `.libPaths()`.  It's recommended to use `set_library()` prior to
+#' `curate()`
+#' @param git_provider String, one of "github" or "gitlab".  Default is NULL.
+#' @param ... Additional arguments to be passed to `install.packages`,
+#' `remotes::install_version`, `remotes::install_github`, and `remotes::install_gitlab`.
+#' Use this to pass authentication variables to git providers.
+#'
+#' @return The user library path
+#' @examples
+#' # Setting user library first
+#' set_library("/dbfs/rk/my_packages")
+#' curate(pkg = "broom")
+#'
+#' # Install version, setting user library
+#' curate(pkg = "broom", version = "0.4.2", lib = set_library("/dbfs/rk/my_old_packages"))
+#'
+#' # Install GitHub
+#' curate(repo = "RafiKurlansik/bricksteR")
+#'
+#' # Install Gitlab
+#' curate(repo = "jimhester/covr")
+#' @export
+curate <- function(pkg,
+                   repo = "https://packagemanager.rstudio.com/all/__linux__/xenial/latest",
+                   version = NULL,
+                   lib = .libPaths()[1],
+                   git_provider = NULL,
+                   ...) {
+
+  # Set up temp directory for installation
+  tmp_dir <- tempfile()
+  dir.create(tmp_dir)
+
+  if(!is.null(version)) {
+    remotes::install_version(pkg, version = version, repos = repo, lib = tmp_dir, ...)
+    system(paste0("cp -r ", tmp_dir, "/* ", lib))
+    cat(c("Version ", version, " of ", pkg, " installed in ", lib))
+  }
+
+  if(!is.null(git_provider)) {
+    git_provider <- tolower(git_provider)
+
+    if(!(git_provider %in% c("gitlab", "github"))) {
+      stop("Check your git provider, only \"gitlab\" or \"github\" are supported")
+    }
+
+    # Install GitHub
+    if(git_provider == "github") {
+
+      remotes::install_github(repo = pkg, lib = tmp_dir, ...)
+      system(paste0("cp -r ", tmp_dir, "/* ", lib))
+      cat(c("\n\nRemote GitHub package installed from ", pkg, " in ", lib))
+
+    } else {
+
+    # Install Gitlab
+    remotes::install_gitlab(repo = pkg, lib = tmp_dir, ...)
+    system(paste0("cp -r ", tmpDir, "/* ", lib))
+    cat(c("\n\nRemote Gitlab package installed from  ", pkg, " in ", lib))
+
+    }
+
+    # Normal installation
+    install.packages(pkgs = pkg, repos = repo, lib = tmp_dir, ...)
+    system(paste0("cp -r ", tmp_dir, "/* ", lib))
+    cat(c("Package: ", pkg, " installed in ", lib))
+
+  # Clean up temp directory
+  system(paste0("rm -r ", tmp_dir))
+
+  }
+}
+
---
+++ R/set_library.R
@@ -0,0 +1,65 @@
+#'
+#' Set the User Library Path
+#'
+#' Set the path for where R will install packages.
+#'
+#' To persist packages on Databricks use a path that begins with `/dbfs/`.
+#' This is designed to be used in conjunction with `bricksteR::curate()`.
+#' For shared clusters where users prefer package isolation, each user should
+#' create their own path for package installation.  If building a central
+#' shared repo of packages, set the path to a common directory.
+#'
+#' @param user_libpath A string representing the path to install packages in DBFS
+#'
+#' @return The user library path
+#' @examples
+#'
+#' path <- "/dbfs/rk/my_packages"
+#'
+#' set_library(lib_path = path)
+#' @export
+set_library <- function(lib_path = "/dbfs/Rlib"){
+
+  # If using default, add random path to avoid package collisions
+  if (lib_path == '/dbfs/Rlib') {
+  user_lib_path <- file.path(lib_path,
+                            round(rnorm(1, sd = 10000)),
+                            getRversion())
+  } else {
+    user_lib_path <- file.path(lib_path, getRversion())
+  }
+
+  if (file.exists(user_lib_path) == F) {
+    dir.create(user_lib_path, recursive = TRUE)
+  }
+
+
+  search_path <- .libPaths()
+  if (user_lib_path %in% search_path) {
+    search_path <- setdiff(search_path, user_lib_path)
+  }
+
+  # adding user specified library path into search paths
+  search_path <- c(user_lib_path, search_path)
+  .libPaths(search_path)
+
+  cat(c("Library: ", user_lib_path, " added. \n\nUse .libPaths() to see all libraries."))
+}
+
+#' @rdname set_library
+#'
+#' @param user_lib_path A string representing the path to be removed from .libPaths()
+#' @return The removed library path
+#' @examples
+#'
+#' remove_library(path)
+#' @export
+remove_library <- function(lib_path){
+
+  search_path <- .libPaths()
+  if (lib_path %in% search_path) {
+    search_path <- setdiff(search_path, lib_path)
+    .libPaths(search_path)
+  }
+  cat(c("Library: ", lib_path, " removed. \n\nUse .libPaths() to see all  libraries."))
+}
